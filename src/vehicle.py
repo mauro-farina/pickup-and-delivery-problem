1efbd44aba3df95877694da096134a490b5be23b
Mauro Farina <maurofarina@protonmail.com>
2024-03-10 10:33:07 +0100

    add depot nodes

--- src/vehicle.py
+++ src/vehicle.py
@@ -1,6 +1,12 @@
+from src.graph import Node
+
+
 class Vehicle:
 
-    def __init__(self, index: int, capacity: int, travel_unit_cost: int = 1):
+    def __init__(self, index: int, origin_depot: Node, destination_depot: Node,
+                 capacity: int, travel_unit_cost: int = 1):
         self.index = index
+        self.origin = origin_depot
+        self.dest = destination_depot
         self.capacity = capacity
         self.travel_unit_cost = travel_unit_cost
