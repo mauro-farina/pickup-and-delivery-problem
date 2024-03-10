b5e4d63fc5066dd5cdc1a1e5f07440cd6fa5fb85
Mauro Farina <maurofarina@protonmail.com>
2024-03-10 10:32:44 +0100

    add index

--- src/request.py
+++ src/request.py
@@ -3,7 +3,8 @@ from src.graph import Node
 
 class Request:
 
-    def __init__(self, pickup: Node, destination: Node, load: int):
+    def __init__(self, index: int, pickup: Node, destination: Node, load: int):
+        self.index = index
         self.pickup = pickup
         self.destination = destination
         self.load = int(load)
