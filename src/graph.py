2b7ef386c0119219e8d4e13faf4f1b6b0dbd1975
Mauro Farina <maurofarina@protonmail.com>
2024-03-17 11:40:27 +0100

    add earliest_time and latest_time for time windows constraints

--- src/graph.py
+++ src/graph.py
@@ -11,10 +11,13 @@ class NodeType(StrEnum):
 
 class Node:
 
-    def __init__(self, index: int, node_type: NodeType, coordinates: tuple[int, int]):
+    def __init__(self, index: int, node_type: NodeType, coordinates: tuple[int, int],
+                 earliest_time: int, latest_time: int):
         self.index = index
         self.type = node_type
         self.coordinates = coordinates
+        self.earliest_time = earliest_time
+        self.latest_time = latest_time
 
     def __eq__(self, other):
         if other.__class__ is self.__class__:
@@ -28,7 +31,7 @@ class Node:
         return hash(self.index)
 
     def __str__(self):
-        return f'{self.type}{self.index}@{self.coordinates}'
+        return f'{self.type}{self.index}@{self.coordinates};E={self.earliest_time};L={self.latest_time}'
 
 
 class Arc:
