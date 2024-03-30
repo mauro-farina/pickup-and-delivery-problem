8b019467d804b6f773a3170f1f2cd5968140bb8d
Mauro Farina <maurofarina@protonmail.com>
2024-03-30 15:20:41 +0100

    refactor with dataclass

--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,5 @@
 from enum import StrEnum
+from dataclasses import dataclass
 
 
 class NodeType(StrEnum):
@@ -9,48 +10,23 @@ class NodeType(StrEnum):
     TRANSFER_STATION = 't'
 
 
+@dataclass(frozen=True)
 class Node:
-
-    def __init__(self, index: int, node_type: NodeType, coordinates: tuple[int, int],
-                 earliest_time: int, latest_time: int):
-        self.index = index
-        self.type = node_type
-        self.coordinates = coordinates
-        self.earliest_time = earliest_time
-        self.latest_time = latest_time
-
-    def __eq__(self, other):
-        if other.__class__ is self.__class__:
-            return (self.index == other.index
-                    and self.type == other.type
-                    and self.coordinates == other.coordinates)
-        else:
-            return False
-
-    def __hash__(self):
-        return hash(self.index)
-
-    def __str__(self):
-        return f'{self.type}{self.index}@{self.coordinates};E={self.earliest_time};L={self.latest_time}'
+    index: int
+    type: NodeType
+    coordinates: tuple[int, int]
+    earliest_time: int
+    latest_time: int
 
 
+@dataclass(frozen=True)
 class Arc:
-
-    def __init__(self, source: Node, destination: Node, cost: float):
-        self.src = source
-        self.dst = destination
-        self.cost = cost
+    src: Node
+    dst: Node
+    cost: float
 
 
+@dataclass(frozen=True)
 class Graph:
-
-    def __init__(self, nodes: set[Node], arcs: set[Arc]):
-        self.nodes = nodes
-        self.arcs = arcs
-
-    def get_arc(self, source_index: int, destination_index: int) -> None | Arc:
-        for arc in self.arcs:
-            if (arc.src.index == source_index
-                    and arc.dst.index == destination_index):
-                return arc
-        return None
+    nodes: set[Node]
+    arcs: set[Arc]
--- src/vehicle.py
+++ src/vehicle.py
@@ -1,24 +1,12 @@
+from dataclasses import dataclass
+
 from src.graph import Node
 
 
+@dataclass(frozen=True)
 class Vehicle:
-
-    def __init__(self, index: int, origin_depot: Node, destination_depot: Node,
-                 capacity: int, travel_unit_cost: int = 1):
-        self.index = index
-        self.origin = origin_depot
-        self.dest = destination_depot
-        self.capacity = capacity
-        self.travel_unit_cost = travel_unit_cost
-
-    def __hash__(self):
-        return hash((self.index, self.origin, self.dest, self.capacity))
-
-    def __eq__(self, other):
-        if other.__class__ is self.__class__:
-            return self.index == other.index and self.origin == other.origin and self.dest == other.dest
-        else:
-            return False
-
-    def __str__(self):
-        return f'vehicle {self.index}: origin={self.origin}, destination={self.dest}, capacity={self.capacity}'
+    index: int
+    origin: Node
+    dest: Node
+    capacity: int
+    travel_unit_cost: int = 1
