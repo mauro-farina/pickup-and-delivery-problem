55036138bf3de629fe77ba464d7a4369818d07de
Mauro Farina <maurofarina@protonmail.com>
2024-03-31 19:05:12 +0200

    refactor with dataclass

--- src/request.py
+++ src/request.py
@@ -1,13 +1,10 @@
 from src.graph import Node
+from dataclasses import dataclass
 
 
+@dataclass(frozen=True)
 class Request:
-
-    def __init__(self, index: int, pickup: Node, destination: Node, load: int):
-        self.index = index
-        self.pickup = pickup
-        self.destination = destination
-        self.load = int(load)
-
-    def __str__(self):
-        return f'request {self.index}: pickup={self.pickup}, destination={self.destination}, load={self.load}'
+    index: int
+    pickup: Node
+    destination: Node
+    load: int
