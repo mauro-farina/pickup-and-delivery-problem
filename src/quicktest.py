d528429dbbcf97d3394a6d2bd33f114fb2e8b7a3
Mauro Farina <maurofarina@protonmail.com>
2024-05-07 14:02:57 +0200

    add argparse for CLI execution

--- src/quicktest.py
+++ src/quicktest.py
@@ -4,9 +4,18 @@ from sampaio import Sampaio
 from utils import get_instance_data
 
 from pathlib import Path
+from typing import Optional
+
+import argparse
+import re
 
 
 def lyu(path: Path) -> None:
+    """
+    Solve the instance found at path with Lyu model, print the result
+    :param path: file containing the instance data
+    :return: Nothing
+    """
     g, v, r = get_instance_data(path)
 
     model = Lyu(g, v, r)
@@ -15,16 +24,26 @@ def lyu(path: Path) -> None:
     print(path.name, '\tLyu \t', model.get_result())
 
 
-def sampaio(path: Path, *, vi: bool = False) -> None:
-    g, v, r = get_instance_data(path, sampaio=not vi)
+def sampaio(path: Path) -> None:
+    """
+    Solve the instance found at path with Sampaio model, print the result
+    :param path: file containing the instance data
+    :return: Nothing
+    """
+    g, v, r = get_instance_data(path, sampaio=True)
 
-    model = Sampaio(g, v, r, vi=vi)
+    model = Sampaio(g, v, r)
     model.optimize()
 
     print(path.name, '\tSampaio\t', model.get_result())
 
 
 def rais(path: Path) -> None:
+    """
+    Solve the instance found at path with Rais model, print the result
+    :param path: file containing the instance data
+    :return: Nothing
+    """
     g, v, r = get_instance_data(path)
 
     model = Rais(g, v, r)
@@ -33,21 +52,61 @@ def rais(path: Path) -> None:
     print(path.name, '\tRais\t', model.get_result())
 
 
-def get_pdpt_path(r: int, k: int, t: int, num: int) -> Path:
+def _get_pdpt_path(r: int, k: int, t: int, num: int) -> Optional[Path]:
     params = f'PDPT-R{r}-K{k}-T{t}'
-    return Path(f'../data/PDPT/{params}/{params}-Q100-{num}.txt')
+    path = Path(f'../data/PDPT/{params}/{params}-Q100-{num}.txt')
+    if path.exists():
+        return path
+    else:
+        return None
 
 
-def get_pdptwt_path(r: int, k: int, t: int, shift: int, d: str, num: int) -> Path:
+def _get_pdptwt_path(r: int, k: int, t: int, shift: int, d: str, num: int) -> Optional[Path]:
     params = f'{r}R-{k}K-{t}T'
-    return Path(f'../data/PDPTWT/{params.replace('-', '')}/{params}-{shift}{d}-{num}.txt')
+    path = Path(f'../data/PDPTWT/{params.replace('-', '')}/{params}-{shift}{d}-{num}.txt')
+    if path.exists():
+        return path
+    else:
+        return None
 
 
 if __name__ == '__main__':
-    p0 = get_pdpt_path(5, 2, 1, 6)
-    # p1 = get_pdptwt_path(3, 4, 4, 180, 'M', 0)
-    rais(p0)
-    lyu(p0)
-    # sampaio(p1, vi=False)
-    # sampaio(p1, vi=True)
-    # lyu(p1)
+    parser = argparse.ArgumentParser(description='Quickly test an instance with the specified model')
+    parser.add_argument('instance', type=str, help='Instance to run, e.g. PDPT-R5-K2-T1-Q100-6')
+    parser.add_argument('model', type=str, help='Model to use')
+
+    args = parser.parse_args()
+
+    model = args.model
+    if model.lower() not in ['lyu', 'sampaio', 'rais']:
+        print('Model must be either Rais, Lyu or Sampaio')
+        exit(1)
+
+    pdpt_instance = r'PDPT-R(\d+)-K(\d+)-T(\d+)-Q100-(\d+)'
+    pdptwt_instance = r'(\d+)R-(\d+)K-(\d+)T-(\d+)(L|M|S)-(\d+)'
+
+    instance = args.instance
+    if instance.endswith('.txt'):
+        instance = instance[:-4]
+
+    path = None
+    if re.match(pdpt_instance, instance):
+        match_res = re.match(pdpt_instance, instance)
+        path = _get_pdpt_path(match_res.group(1), match_res.group(2), match_res.group(3), match_res.group(4))
+    elif re.match(pdptwt_instance, instance):
+        match_res = re.match(pdptwt_instance, instance)
+        path = _get_pdptwt_path(match_res.group(1), match_res.group(2), match_res.group(3), match_res.group(4),
+                                match_res.group(5), match_res.group(6))
+
+    if not path:
+        print('Instance does not exist')
+        exit(1)
+
+    if model.lower() == 'rais' and 'PDPTWT' not in path.parts:
+        rais(path)
+    elif model.lower() == 'sampaio' and 'PDPTWT' in path.parts:
+        sampaio(path)
+    elif model.lower() == 'lyu':
+        lyu(path)
+    else:
+        print(f'{model.title()} model cannot solve {instance}')
